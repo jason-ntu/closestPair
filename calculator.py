9d3433c26a3fb4fe33005295a075fecd0428aa4a
Jason <df41022@gmail.com>
2023-03-22 20:30:31 +0800

    test

--- calculator.py
+++ calculator.py
@@ -1,25 +1,31 @@
 import math
 
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, x = None, y = None):
+        if x is not None:
+            self.x = float(x)
+        if y is not None:
+            self.y = float(y)
 
     def x_between(self, x0, x1):
         return x0 <= self.x and self.x <= x1
 
-    def dist(p0, p1):
+    def dist(self, p0, p1):
         return math.sqrt(math.pow(p0.x - p1.x, 2) + math.pow(p0.y - p1.y, 2))
+    
 
 class Calculator:
+    def __init__(self):
+        return
+
     def brute_force_closest_distance(self, point_list):
         n = len(point_list)
         if n == 1:
             return -1
-        d = Point.dist(point_list[0], point_list[1])
+        d = Point().dist(point_list[0], point_list[1])
         for i in range(n):
             for j in range(i + 1, n):
-                d = min(d, Point.dist(point_list[i], point_list[j]))
+                d = min(d, Point().dist(point_list[i], point_list[j]))
         return d
 
     def closest_distance(self, point_list):
@@ -44,30 +50,40 @@ class Calculator:
         for i in range(len(strip_list)):
             for j in range(1, 8):
                 if i+j < len(strip_list):            
-                    d = min(d, Point.dist(strip_list[i], strip_list[i+j]))
+                    d = min(d, Point().dist(strip_list[i], strip_list[i+j]))
         return d
 
 class Converter:
-    def input_to_cases(self, input):
-        points = []
+    def __init__(self):
+        return
+    
+    def lines_to_cases(self, input):
+        cases = []
 
         lines = input.split("\n")
-        left_number_of_cases = lines[0]
+        left_number_of_cases = int(lines[0])
         line_index = 1
     
         while left_number_of_cases > 0:
-            number_of_points = lines[line_index]
+            points = []
+            number_of_points = int(lines[line_index])
             next_line_index = line_index + number_of_points + 1
-            for line in lines[line_index:next_line_index]:
-                points.append(self.text_to_point(line))
+            for line in lines[line_index+1 : next_line_index]:
+                points.append(self.line_to_point(line))
+            cases.append(points)
             line_index = next_line_index
             left_number_of_cases -= 1
-        return points
+        return cases
     
-    def text_to_point(self, text):
+    def line_to_point(self, text):
         cordinate = text.split(" ")
         return Point(cordinate[0], cordinate[1])
 
+def main():
+    print("Start executing...")
+    cases = Converter().lines_to_cases(input())
+    for case in cases:
+        Calculator().closest_distance(case)
 
 if __name__ == '__main__':
-    Calculator.closest_distance()
+    main()
--- makefile
+++ makefile
@@ -4,7 +4,7 @@ CACHE := ./__pycache__  .coverage ./htmlcov
 .PHONY: clean
 
 coverage:
-	@flake8 --max-line-length=127 $(FILE_NAME)
+	@flake8 --max-line-length=255 $(FILE_NAME)
 	@coverage run -m unittest $(FILE_NAME)
 
 report: .coverage
